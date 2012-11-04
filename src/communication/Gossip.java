41869727abaeaffec184985d9e90b633bc6facda
naveed <mnaveed29@gmail.com>
2012-11-04 13:38:47 -0600

    Added Gossip.java class in communication package.

--- src/communication/Gossip.java
+++ src/communication/Gossip.java
@@ -4,8 +4,8 @@ package communication;
  * Created with IntelliJ IDEA.
  * User: naveed
  * Date: 11/4/12
- * Time: 11:56 AM
+ * Time: 1:37 PM
  * To change this template use File | Settings | File Templates.
  */
-public class gossip {
+public class Gossip {
 }
