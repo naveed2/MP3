66ec549c161e3a76f8a58ed8e776439f20166c04
naveed <mnaveed29@gmail.com>
2012-11-04 12:32:54 -0600

    Added Gossip.java

---
+++ src/communication/gossip.java
@@ -0,0 +1,11 @@
+package communication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: naveed
+ * Date: 11/4/12
+ * Time: 11:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class gossip {
+}
