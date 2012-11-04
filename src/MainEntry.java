0abe10dfe42638cc7ae5ebf707f635767e0592c1
conanhjj <conanhjj@gmail.com>
2012-11-04 01:35:39 -0600

    Add base lib.

--- src/MainEntry.java
+++ src/MainEntry.java
@@ -1,6 +1,14 @@
+import org.apache.log4j.PropertyConfigurator;
 
 public class MainEntry {
     public static void main(String[] args) {
+
+        log4jConfigure();
+
         System.out.println("test");
+    }
+
+    private static void log4jConfigure() {
+        PropertyConfigurator.configure("log4j.properties");
     }
 }
