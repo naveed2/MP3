2f574a7251577691b79f9db6e8cef9a7ae559379
naveed <mnaveed29@gmail.com>
2012-11-04 13:41:15 -0600

    Added communication:Gossip.java

--- src/communication/Gossip.java
+++ src/communication/Gossip.java
@@ -4,8 +4,24 @@ package communication;
  * Created with IntelliJ IDEA.
  * User: naveed
  * Date: 11/4/12
- * Time: 1:37 PM
+ * Time: 1:22 PM
  * To change this template use File | Settings | File Templates.
  */
+
+import membership.MemberList;
 public class Gossip {
+
+    private Integer noOfTargets;
+
+    private MemberList memberList;
+
+
+
+
+    void setNoOfTargets(Integer noOfTargets){
+        this.noOfTargets = noOfTargets;
+    }
+
+
+
 }
