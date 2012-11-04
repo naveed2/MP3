3dd7d4328b47425b7336211416a0140ab2617ed1
naveed <mnaveed29@gmail.com>
2012-11-04 13:52:17 -0600

    Changed Message.java

--- src/membership/MemberList.java
+++ src/membership/MemberList.java
@@ -1,11 +1,23 @@
 package membership;
 
-import communication.*;
+import communication.Messages.ProcessIdentifier;
 
 import java.util.LinkedList;
 
 public class MemberList {
 
-    private LinkedList<Messages.ProcessIdentifier> list;
+    private LinkedList<ProcessIdentifier> list;
+
+    void removeProcess(ProcessIdentifier processIdentifier){
+        this.list.remove(processIdentifier);
+    }
+
+    void addProcess(ProcessIdentifier processIdentifier){
+        this.list.add(processIdentifier);
+    }
+
+    MemberList getMemberList(){
+        return this.list;
+    }
     
 }
