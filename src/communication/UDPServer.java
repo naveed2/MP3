695023af39f312b3c05b3d649fb8cfd6cbd20149
conanhjj <conanhjj@gmail.com>
2012-11-09 03:11:55 -0600

    Add UDPServe code. It hasn't been tested yet.

--- src/communication/UDPServer.java
+++ src/communication/UDPServer.java
@@ -1,11 +1,77 @@
 package communication;
 
-/**
- * Created with IntelliJ IDEA.
- * User: jhu14
- * Date: 11/4/12
- * Time: 2:53 PM
- * To change this template use File | Settings | File Templates.
- */
+import membership.Proc;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static communication.Messages.*;
+
 public class UDPServer {
+    private Integer udpPort;
+    private AtomicBoolean shouldStop;
+    private DatagramSocket serverSocket;
+    private static Logger logger = Logger.getLogger(UDPServer.class);
+    private Proc proc;
+
+    private static final Integer BUFFER_SIZE = 4096;
+
+    public UDPServer(Integer udpPort) {
+        this.udpPort = udpPort;
+        shouldStop = new AtomicBoolean(false);
+    }
+
+
+
+    public boolean start() {
+
+        try {
+            serverSocket = new DatagramSocket(udpPort);
+        } catch (SocketException e) {
+            logger.error("udp server binding port error", e);
+            return false;
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startListening();
+            }
+        }).start();
+        return true;
+    }
+
+    private void startListening() {
+
+        while(!shouldStop.get()) {
+            DatagramPacket packet = new DatagramPacket(new byte[BUFFER_SIZE], BUFFER_SIZE);
+            try {
+                serverSocket.receive(packet);
+                int len = packet.getLength();
+                byte[] bytes = new byte[len];
+                System.arraycopy(packet.getData(), 0, bytes, 0, len);
+                logger.debug("Received packet: " + Arrays.toString(packet.getData()));
+//                Message message = Message.parseFrom(bytes);
+//                logger.debug("Received Message: " + message.toString());
+
+            } catch (IOException e) {
+                if(e.getMessage().equals("socket close")) {
+                    break;
+                } else {
+                    logger.error("udp server socket exception" + e);
+                }
+            }
+
+        }
+    }
+
+
+    public void setProc(Proc proc) {
+        this.proc = proc;
+    }
 }
--- src/membership/Proc.java
+++ src/membership/Proc.java
@@ -3,6 +3,7 @@ package membership;
 
 import communication.ProcessIdentifierFactory;
 import communication.TCPServer;
+import communication.UDPServer;
 import org.apache.log4j.Logger;
 
 import java.net.InetAddress;
@@ -13,7 +14,11 @@ import static communication.Messages.*;
 
 public class Proc {
     private Integer tcpPort;
+    private Integer udpPort;
     private TCPServer tcpServer;
+    private UDPServer udpServer;
+    private Boolean isTCPServerStarted;
+    private Boolean isUDPServerStarted;
     private Logger logger = Logger.getLogger(Proc.class);
     private Integer timeStamp;
     private String id;
@@ -24,7 +29,10 @@ public class Proc {
         this.timeStamp = 0;
         this.id = UUID.randomUUID().toString();
         this.tcpPort = tcpPort;
+        this.udpPort = tcpPort + 1; // UDPPort is always set to TCPPort+1
         memberList = new MemberList();
+
+        this.isTCPServerStarted = this.isUDPServerStarted = false;
     }
 
     private void initIdentifier() {
@@ -45,15 +53,34 @@ public class Proc {
         initIdentifier();
         memberList.add(identifier);
 
+        //TODO: before init new serve, old server should be closed. 
+        initTCPServer();
+        initUDPServer();
+    }
+
+    public void initTCPServer() {
         tcpServer = new TCPServer(tcpPort);
         tcpServer.setProc(this);
 
         if(tcpServer.start()) {
-            System.out.println("TCP Server starts successfully");
-            logger.info("TCP Server starts successfully");
+            isTCPServerStarted = true;
+            logger.info("TCP Server starts successfully, listening to port " + tcpPort);
         } else {
             System.err.println("TCP Server starts failed, please check configuration");
             logger.fatal("TCP Server starts failed");
+        }
+    }
+
+    public void initUDPServer() {
+        udpServer = new UDPServer(udpPort);
+        udpServer.setProc(this);
+
+        if(udpServer.start()) {
+            isUDPServerStarted = true;
+            logger.info("UDP Server starts successfully, listening to port " + udpPort);
+        } else {
+            System.err.println("UDP Server starts failed, please check configuration");
+            logger.fatal("UDP Server starts failed");
         }
     }
 
